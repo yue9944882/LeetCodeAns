631c77918aaea660c678d1d5c03af372689f675d
yue9944882 <291271447@qq.com>
2016-05-02 14:51:19 +0800

    Ans 111
    Signed-off-by: yue9944882 <291271447@qq.com>

---
+++ src/main/java/com/kimmin/oj/leetcode/Ans111.java
@@ -0,0 +1,26 @@
+package com.kimmin.oj.leetcode;
+
+/**
+ * Created by kimmin on 5/2/16.
+ */
+public class Ans111 {
+//    int recFindMin(TreeNode* parent){
+//        int lmin = 999999999;
+//        int rmin = 999999999;
+//        if(parent->left == NULL && parent->right == NULL) return 1;
+//        if(parent->left != NULL) lmin = recFindMin(parent->left);
+//        if(parent->right != NULL) rmin = recFindMin(parent->right);
+//        return (lmin < rmin? lmin:rmin) + 1;
+//    }
+//
+//    class Solution {
+//        public:
+//        int minDepth(TreeNode* root) {
+//            if(root == NULL) return 0;
+//            return recFindMin(root);
+//        }
+//    };
+    /** Cpp **/
+
+
+}
