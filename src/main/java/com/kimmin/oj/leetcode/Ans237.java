df523c86af0a43db45d35cafbecd0b118824ad91
yue9944882 <291271447@qq.com>
2016-05-02 18:14:10 +0800

    Ans 237
    Signed-off-by: yue9944882 <291271447@qq.com>

---
+++ src/main/java/com/kimmin/oj/leetcode/Ans237.java
@@ -0,0 +1,31 @@
+package com.kimmin.oj.leetcode;
+
+import java.util.*;
+
+/**
+ * Created by kimmin on 5/2/16.
+ */
+public class Ans237 {
+    public class ListNode {
+        public ListNode next;
+        public int val;
+    }
+    public void deleteNode(ListNode node) {
+        ListNode curNode = node;
+        ListNode nextNode = node.next;
+        if(nextNode == null){
+            /** This is impossible! **/
+        }
+        while(true){
+            curNode.val = nextNode.val;
+            if(nextNode.next == null){
+                curNode.next = null;
+                break;
+            }
+            curNode = nextNode;
+            nextNode = nextNode.next;
+
+        }
+        curNode.next = null;
+    }
+}
