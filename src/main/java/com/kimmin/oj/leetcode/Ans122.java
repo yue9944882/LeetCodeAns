c4b476150ff249d49b47c4c9895e83cca8720698
yue9944882 <291271447@qq.com>
2016-05-03 17:51:20 +0800

    Ans 122
    Signed-off-by: yue9944882 <291271447@qq.com>

---
+++ src/main/java/com/kimmin/oj/leetcode/Ans122.java
@@ -0,0 +1,25 @@
+package com.kimmin.oj.leetcode;
+
+/**
+ * Created by kimmin on 5/3/16.
+ */
+public class Ans122 {
+    public class Solution {
+        public int maxProfit(int[] prices) {
+            if(prices.length == 0) return 0;
+            int profit = 0;
+            // True means can only buy stock, on the contrary
+            boolean b = true;
+            int[] priceDiff = new int[prices.length - 1];
+            for(int i=0; i<prices.length - 1; i++){
+                priceDiff[i] = prices[i + 1] - prices[i];
+            }
+            for(int i=0; i<priceDiff.length; i++){
+                if(priceDiff[i] > 0){
+                    profit += priceDiff[i];
+                }
+            }
+            return profit;
+        }
+    }
+}
