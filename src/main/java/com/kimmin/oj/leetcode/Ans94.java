758666f0a2e9b6a4bebf49821632de46e9317160
yue9944882 <291271447@qq.com>
2016-05-03 22:30:39 +0800

    Ans 94
    Signed-off-by: yue9944882 <291271447@qq.com>

--- src/main/java/com/kimmin/oj/leetcode/Ans94.java
+++ src/main/java/com/kimmin/oj/leetcode/Ans94.java
@@ -6,7 +6,16 @@ import java.util.List;
 /**
  * Created by kimmin on 5/3/16.
  */
-public class Ans144 {
+public class Ans94 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
     /**
      * Definition for a binary tree node.
      * public class TreeNode {
@@ -17,15 +26,16 @@ public class Ans144 {
      * }
      */
     public class Solution {
-        public List<Integer> preorderTraversal(Ans100.TreeNode root) {
+        public List<Integer> inorderTraversal(Ans100.TreeNode root) {
             if(root == null) return new LinkedList<Integer>();
             List<Integer> list = new LinkedList<Integer>();
             recPreorderTraversal(root, list);
             return list;
         }
         public void recPreorderTraversal(Ans100.TreeNode parent, List<Integer> list){
-            list.add(parent.val);
+
             if(parent.left != null) recPreorderTraversal(parent.left, list);
+            list.add(parent.val);
             if(parent.right != null) recPreorderTraversal(parent.right, list);
         }
     }
