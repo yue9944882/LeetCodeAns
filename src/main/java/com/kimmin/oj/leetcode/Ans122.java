2a5fc47c28451c6482a946b4375c002f8f3270f3
yue9944882 <291271447@qq.com>
2016-05-03 18:07:23 +0800

    Ans 242
    Signed-off-by: yue9944882 <291271447@qq.com>

--- src/main/java/com/kimmin/oj/leetcode/Ans122.java
+++ src/main/java/com/kimmin/oj/leetcode/Ans122.java
@@ -20,6 +20,7 @@ public class Ans122 {
                 }
             }
             return profit;
+
         }
     }
 }
---
+++ src/main/java/com/kimmin/oj/leetcode/Ans242.java
@@ -0,0 +1,46 @@
+package com.kimmin.oj.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by kimmin on 5/3/16.
+ */
+public class Ans242 {
+    public class Solution {
+        public boolean isAnagram(String s, String t) {
+            if(s.length() != t.length()) return false;
+            Map<Character, Integer> map1 = new HashMap<Character, Integer>();
+            Map<Character, Integer> map2 = new HashMap<Character, Integer>();
+            for(int i=0; i<s.length(); i++){
+                if(!map1.containsKey(s.charAt(i))){
+                    map1.put(s.charAt(i), 1);
+                }else{
+                    int val = map1.get(s.charAt(i));
+                    map1.put(s.charAt(i), val + 1);
+                }
+
+                if(!map2.containsKey(t.charAt(i))){
+                    map2.put(t.charAt(i), 1);
+                }else{
+                    int val = map2.get(t.charAt(i));
+                    map2.put(t.charAt(i), val + 1);
+                }
+            }
+
+            for(Character c: map1.keySet()){
+                Integer i1 = map1.get(c);
+                Integer i2 = map2.get(c);
+                if(i1 == null || i2 == null) return false;
+                if(i1.intValue() != i2.intValue()) return false;
+            }
+            for(Character c: map2.keySet()){
+                Integer i1 = map1.get(c);
+                Integer i2 = map2.get(c);
+                if(i1 == null || i2 == null) return false;
+                if(i1.intValue() != i2.intValue()) return false;
+            }
+            return true;
+        }
+    }
+}
